f0c3330850e8af75e3e2cd1e03164a879739c506
BryceAC <39605594+Bryce-AC@users.noreply.github.com>
2021-07-15 17:17:51 +0930

    Removed garbage

--- code/main/main.ino
+++ code/main/main.ino
@@ -3,67 +3,43 @@
 
 MLX90621 tempSensor;
 
-unsigned long measurementID = 0;
-String packet = "";
-
-/*
-   Data format explanations:
-
-   {"measurementID":N, "pointID":X, "temp":t}
-
-   pointID - point of measurement from 0-64
-
-   Shortened:
-
-   {"m":N, "p":X, "t":t}
-
-   Pinout:
-
-   Sensor:   Feather
-   GND - GND
-   VCC - 3.3v
-   SCL - SCL
-   SDA - SDA
-
-*/
-
-/* User config variables */
-
-#define SerialSpeed 115200 //you can change boud rate
-#define Pause 100 //pause between measurements in ms (refreshrate)
-
 void setup() {
 
-  Serial.begin(SerialSpeed);
-
+  Serial.begin(115200);
   Serial.println("\nTire temperature sensor starting...");
-
   Wire.begin();// Starting I2C
-
   Serial.println("Starting temp sensor");
-
+  //initialise temperature sensor with refrate=16
   tempSensor.initialise(16);
-
   delay(100);
 
 }
 
-void loop() {
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(0x70);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
 
+void loop() {
+  //select port zero on multiplexer
+  tcaselect(0);
+  //get updated measurements
   tempSensor.measure(true);
 
-  for (int y = 0; y < 4; y++) { // go through all the rows
-    for (int x = 0; x < 16; x++) { // go through all the columns
-      int point = y*16 + x;
-      double tempAtXY = tempSensor.getTemperature(point); // extract the temperature at position x/y
+  //sensor has 4 rows of 16 data points. grab first row for now.
+  int row = 0;
+  float temp_pkt[16];
 
-      packet = "{\"m\":" + String(measurementID) + "," + "\"p\":" + String(point) + "," + "\"t\":" + String(tempAtXY) + "}";
-
-      Serial.println(packet);// if you dont want to handle \n character in data parser on MoTeC side change "println" to "print"
-    }
+  //iterate over each column in the data and store into temp_pkt
+  for (int col = 0; col < 16; col++){
+    int point = row*16 + col;
+    temp_pkt[col] = tempSensor.getTemperature(point);
+    Serial.print(temp_pkt[col]);
+    Serial.print(" ");
   }
-
-  delay(Pause);
-  measurementID++;
-
+  Serial.print("\n");
+  
 }
